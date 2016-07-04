4bb560356d2adac23010ceb35859898ab6295568
Jordan Cauley <jordan@cauley.co>
2016-07-04 14:51:44 -0400

    working post grab
    Fetches to soon, needs a timeout or method to fetch until content is available

---
+++ api/controllers/PostController.js
@@ -0,0 +1,23 @@
+/**
+ * PostController
+ *
+ * @description :: Server-side logic for managing Posts
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	// getPosts: function(params, callback){
+	//
+	// 	Posts.find().exec(function(err, posts){
+	// 		if(err){
+	// 			res.json(err);
+	// 		}
+	// 		if(posts){
+	// 			res.json(posts);
+	// 		}
+	// 	});
+
+	}
+
+};
--- api/services/Slack.js
+++ api/services/Slack.js
@@ -2,33 +2,92 @@ var Slack = require('slack');
 
 module.exports = {
 
-  listen: function(user){
+  listen: function(){
 
-		var bot = Slack.rtm.client()
-		var token = user.slackToken;
+    var bot = Slack.rtm.client()
+    var token = sails.config.secrets.slack.token;
 
-		// logs: ws, started, close, listen, etc... in addition to the RTM event handler methods
-		console.log(Object.keys(bot))
+    // logs: ws, started, close, listen, etc... in addition to the RTM event handler methods
+    console.log(Object.keys(bot))
 
-		// do something with the rtm.start payload
-		bot.started(function(payload) {
-		  console.log('payload from rtm.start', payload)
-		})
+    // do something with the rtm.start payload
+    bot.started(function(payload) {
+      // console.log('payload from rtm.start', payload)
+    });
 
-		// respond to a user_typing message
-		bot.user_typing(function(msg) {
-		  console.log('several people are coding', msg)
-		});
+    // respond to a user_typing message
+    // bot.user_typing(function(msg) {
+    //   console.log('several people are coding', msg);
+    // });
 
-		bot.message(function(msg){
-			console.log(msg);
-		})
+    bot.message(function(msg){
 
-		// start listening to the slack team associated to the token
-		bot.listen({
-			token:token
-		});
+      console.log(msg);
 
-	}
+      if( (msg.subtype === 'file_share') && (msg.file.pretty_type === 'Post' ) ){
+
+        Slack.files.info({token: token, file: msg.file.id}, function(err, data){
+          if(err){
+            cosole.log(err);
+          }
+          if(data){
+            console.log(data);
+
+            Post.findOrCreate({fileId: msg.file.id}, {
+              postContent: data.content_html,
+              author: msg.user,
+              fileId: msg.file.id,
+              fileData: msg.file
+            })
+
+            Post.findOne({fileId: msg.file.id}, function(err, post){
+              if(err){
+                console.log(err);
+              }
+              if(post){
+                Post.update(post.id, {
+                  channel : data.channel,
+                  postContent: data.content_html,
+                  author: msg.user,
+                  fileId: msg.file.id,
+                  fileData: msg.file,
+                  messageData: msg
+                }, function(err, update){
+                  if(err){
+                    console.log(err);
+                  }
+                });
+              } else {
+                Post.create({
+                  postContent: data.content_html,
+                  author: msg.user,
+                  fileId: msg.file.id,
+                  fileData: msg.file
+                }, function(err, newPost){
+                  if(err){
+                    console.log(err);
+                  }
+                });
+              }
+            });
+
+          }
+        });
+
+      }
+
+
+
+      console.log(msg);
+
+
+    });
+
+    // start listening to the slack team associated to the token
+    bot.listen({
+      token:token
+    });
+
+  }
 
 };
